dc403926736bd5d4903824c0900bc4ae03edee70
Vladimir Udachin <rasnod2018@mail.ru>
2021-01-28 14:13:47 +0300

    Initial Commit

---
+++ AvitoTech-task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoTech-task
+//
+//  Created by Vladimir Udachin on 28.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
